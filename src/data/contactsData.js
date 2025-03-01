bb5ee29e67d291795f18556ed674a3e71cb6a44e
IwamotoSuuky <c.front.dev.eg@gmail.com>
2025-03-02 01:47:06 +0900

    [main]: update contact info

--- src/data/contactsData.js
+++ src/data/contactsData.js
@@ -1,7 +1,4 @@
 export const contactsData = {
-    email: 'msimek.test@gmail.com',
-    phone: '022-485-7294',
+    email: 'iwamoto.suuky@gmail.com',
     address: '宮城県仙台市青葉区台原1丁目2-3',
-
-    sheetAPI: ''
 }
